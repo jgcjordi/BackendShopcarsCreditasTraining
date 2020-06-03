69df156f88874ac6f2f78d8981d1a0da43f150c9
Albert Pla <alplsan@epsa.upv.es>
2020-06-03 15:22:26 +0200

    vehiculos añadidos

--- src/main/kotlin/com/creditas/backend/shopcars/ShopCarsApplication.kt
+++ src/main/kotlin/com/creditas/backend/shopcars/ShopCarsApplication.kt
@@ -77,13 +77,17 @@ class OnBoot(
 
         val bmw = brandService.saveBrand(Brand(name = "BMW"))
         val x1 = modelService.saveModel(Model(name = "X1", brand = bmw))
+        val s1 = modelService.saveModel(Model(name = "S1", brand = bmw))
+        val m3 = modelService.saveModel(Model(name = "M3", brand = bmw))
         val carX1 = carService.saveCar(Car(number_plate = "5378 DTG", model = x1, km = 1200, price = 24000F, color = "Blanco", url_image ="https://www.topgear.es/sites/topgear.es/public/styles/855/public/dc/fotos/BMW-X1-2016-D01.jpg?itok=xea3DBnS"))
+        val carS1 = carService.saveCar(Car(number_plate = "4528 BWG", model = s1, km = 24600, price = 14000F, color = "Blanco", url_image ="https://img.motor16.com/modelos/bmw-serie-1.jpg"))
+        val carM3 = carService.saveCar(Car(number_plate = "7524 ABS", model = m3, km = 15000, price = 44000F, color = "Azul", url_image ="https://cdn.topgear.es/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2019/09/bmw-m3-2020.jpg?itok=AEZtVXp5"))
 
         val nissan = brandService.saveBrand(Brand(name = "Nissan"))
         val qashqai = modelService.saveModel(Model(name = "QASHQAI", brand = nissan))
         val juke = modelService.saveModel(Model(name = "Juke", brand = nissan))
         val carQashqai = carService.saveCar(Car(number_plate = "2475 EJR", model = qashqai, km = 3500, price = 21000F, color = "Blanco", url_image ="https://a.ccdn.es/cnet/contents/media/nissan/qashqai/1234353.jpg/0_122_1280_842//937x624cut/"))
-        val carJuke = carService.saveCar(Car(number_plate = "2565 LKR", model = juke, km = 0, price = 25000F, color = "Rojo", url_image ="https://www.topgear.es/sites/topgear.es/public/styles/855/public/dc/fotos/BMW-X1-2016-D01.jpg?itok=xea3DBnS"))
+        val carJuke = carService.saveCar(Car(number_plate = "2565 LKR", model = juke, km = 0, price = 25000F, color = "Rojo", url_image ="https://imagenes-cdn.autofacil.es/multimedia/fotos/2019/10/09/170354/preview2col_mg.jpg?t=1570621209000"))
 
         val user1 = customerService.saveCustomer(Customer(purchaser_car = mutableListOf(carMustang), name = "Admin", surname = "Admin", identification = "1234A", birthday = LocalDate.of(2017, 1, 13), email = "admin@admin.com", password = "admin"))
 
